0e886e715de1b6793dce81c8a7ce347c4c29bbf7
Umair Butt <umair.butt@hotmail.co.uk>
2017-02-22 01:22:03 +0000

    Completed chapter 5

---
+++ src/Unsupervised/MachineLearning.Unsupervised/PCA.fsx
@@ -0,0 +1,92 @@
+﻿#I @"..\..\packages\"
+#r @"MathNet.Numerics.3.17.0\lib\net40\MathNet.Numerics.dll"
+#r @"MathNet.Numerics.FSharp.3.17.0\lib\net40\MathNet.Numerics.FSharp.dll"
+#load @"FSharp.Charting.0.90.14\FSharp.Charting.fsx"
+#load @"PCA.fs"
+
+open System
+open System.IO
+open MathNet
+open MathNet.Numerics.LinearAlgebra
+open MathNet.Numerics.Statistics
+open FSharp.Charting
+open Unsupervised.PCA
+
+let folder = Path.Combine(__SOURCE_DIRECTORY__, "..", "Data")
+let file = "userprofiles-toptags.txt"
+
+let headers, observations = 
+    let raw = 
+        Path.Combine(folder, file) 
+        |> File.ReadAllLines
+
+    // first column is headers, first col is UuerID
+    let headers = (raw.[0].Split(',')).[1..]
+    let observations = 
+        raw.[1..]
+        |> Array.map ((fun line -> (line.Split ',').[1..]) >> (Array.map float))
+
+    headers, observations
+
+let correlations =
+    observations
+    |> Matrix.Build.DenseOfColumnArrays
+    |> Matrix.toRowArrays
+    |> Correlation.PearsonMatrix
+
+let feats = headers.Length
+let correlated = 
+    seq {
+        for col in 0..(feats-1) do
+            for row in (col+1)..(feats-1) do
+                yield correlations.[col,row], headers.[col], headers.[row]
+    }
+    |> Seq.sortByDescending (fun (corr, _, _) -> abs corr)
+    |> Seq.take 20
+    |> Seq.iter (fun (corr, f1, f2) ->
+        printfn "%s %s : %.2f" f1 f2 corr)
+
+let normalized = normalize (headers.Length) observations
+let (eValues, eVectors), projector = pca normalized
+
+let total = eValues |> Seq.sumBy (fun x -> x.Magnitude)
+eValues
+|> Vector.toList
+|> List.rev // math.NET returns eigenvalues and eigenvectors in ascending eigenvalue order. Customary to look at features in descending order
+|> List.scan (fun (percent, cumul) value ->
+    let percent = 100. * value.Magnitude / total
+    let cumul = cumul + percent
+    (percent, cumul)) (0., 0.)
+|> List.tail
+|> List.iteri (fun i (p, c) -> printfn "Feat %2i: %.2f%% (%.2f%%)" i p c)
+
+let principalComponent comp1 comp2 =
+    let title = sprintf "Component %i vs %i" comp1 comp2
+    let features = headers.Length
+    let coords = Seq.zip (eVectors.Column(features - comp1)) (eVectors.Column(features-comp2))
+    Chart.Point (coords, Title = title, Labels = headers, MarkerSize = 7)
+    |> Chart.WithXAxis(Min = -1.0, Max = 1.0,
+        MajorGrid = ChartTypes.Grid(Interval = 0.25),
+        LabelStyle = ChartTypes.LabelStyle(Interval = 0.25),
+        MajorTickMark = ChartTypes.TickMark(Enabled = false))
+    |> Chart.WithYAxis(Min = -1.0, Max = 1.0,
+        MajorGrid = ChartTypes.Grid(Interval = 0.25),
+        LabelStyle = ChartTypes.LabelStyle(Interval = 0.25),
+        MajorTickMark = ChartTypes.TickMark(Enabled = false))
+
+let projections comp1 comp2 =
+    let title = sprintf "Component %i vs %i" comp1 comp2
+    let features = headers.Length
+    let coords = 
+        normalized
+        |> Seq.map projector
+        |> Seq.map (fun obs -> obs.[features - comp1], obs.[features-comp2])
+    Chart.Point (coords, Title = title)
+    |> Chart.WithXAxis(Min = -200.0, Max = 500.0,
+        MajorGrid = ChartTypes.Grid(Interval = 100.),
+        LabelStyle = ChartTypes.LabelStyle(Interval = 100.),
+        MajorTickMark = ChartTypes.TickMark(Enabled = false))
+    |> Chart.WithYAxis(Min = -200.0, Max = 500.0,
+        MajorGrid = ChartTypes.Grid(Interval = 100.),
+        LabelStyle = ChartTypes.LabelStyle(Interval = 100.),
+        MajorTickMark = ChartTypes.TickMark(Enabled = false))
---
+++ src/Unsupervised/MachineLearning.Unsupervised/Recommender.fsx
@@ -0,0 +1,97 @@
+﻿open System
+open System.IO
+
+let folder = Path.Combine(__SOURCE_DIRECTORY__, "..", "Data")
+let file = "userprofiles-toptags.txt"
+
+let headers, observations = 
+    let raw = 
+        Path.Combine(folder, file) 
+        |> File.ReadAllLines
+
+    // first column is headers, first col is UuerID
+    let headers = (raw.[0].Split(',')).[1..]
+    let observations = 
+        raw.[1..]
+        |> Array.map ((fun line -> (line.Split ',').[1..]) >> (Array.map float))
+        |> Array.filter (fun x -> Array.sum x > 0.)
+
+    headers, observations
+
+let scale (row:float[]) =
+    let min = row |> Array.min
+    let max = row |> Array.max
+    if min = max then row
+    else row |> Array.map (fun x -> (x - min) / (max - min))
+
+let test = observations.[..99] |> Array.map scale
+let train = observations.[100..] |> Array.map scale
+
+let distance (row1:float[]) (row2:float[]) =
+    (row1, row2)
+    ||> Array.map2 (fun x y -> pown (x - y) 2)
+    |> Array.sum
+
+let similarity (row1:float[]) (row2:float[]) =
+    1. / (1. + distance row1 row2)
+
+let split (row:float[]) =
+    row.[..19], row.[20..]
+
+let weights (values:float[]) =
+    let total = values |> Array.sum
+    values
+    |> Array.map (fun x -> x / total)
+
+let predict (row:float[]) =
+    let known, unknown = row |> split
+    let similarities =
+        train
+        |> Array.map (fun example ->
+            let common, _ = example |> split
+            similarity known common)
+        |> weights
+    [|  
+        for i in 20 .. 29 ->
+            let column = train |> Array.map (fun x -> x.[i])
+            let prediction =
+                (similarities, column)
+                ||> Array.map2 (*)//(fun s v -> s * v)
+                |> Array.sum
+            prediction
+    |]
+
+let targetTags = headers.[20..]
+predict test.[0] |> Array.zip targetTags
+test.[0] |> split |> snd // actual
+
+let validation =
+    test
+    |> Array.map (fun obs ->
+        let actual = obs |> split |> snd
+        let predicted = obs |> predict
+        let recommended, observed = 
+            Array.zip predicted actual
+            |> Array.maxBy fst
+        
+        if observed > 0. then 1. else 0.)
+    |> Array.average
+    |> printfn "Correct calls: %f"
+
+// naive recommendation
+let baseline = 
+    let averages = [|
+        for i in 20 .. 29 ->
+            train |> Array.averageBy (fun row -> row.[i]) |]
+
+    test
+    |> Array.map (fun obs ->
+        let actual = obs |> split |> snd
+        let predicted = averages
+        let recommended, observed = 
+            Array.zip predicted actual
+            |> Array.maxBy fst
+        
+        if observed > 0. then 1. else 0.)
+    |> Array.average
+    |> printfn "Correct calls: %f"
