a7e6988612c5ea01521fe189e948966d38fe3892
Umair Butt <umair.butt@hotmail.co.uk>
2017-02-23 23:04:28 +0000

    Added chapter 6 titanic solution

---
+++ src/TreesAndForests/Titanic/Entropy.fsx
@@ -0,0 +1,86 @@
+﻿#I @"..\..\packages\"
+#r @"FSharp.Data.2.3.2\lib\net40\FSharp.Data.dll"
+
+open FSharp.Data
+
+[<Literal>]
+let path = "../Data/titanic.csv"
+
+type Titanic = CsvProvider<path>
+type Passenger = Titanic.Row
+
+let dataset = Titanic.GetSample()
+
+let hasData extractFeature = extractFeature >> Option.isSome
+
+let entropy label data = 
+    let size = data |> Seq.length
+    data 
+    |> Seq.countBy label 
+    |> Seq.map (fun (_,count) -> float count / float size)
+    |> Seq.sumBy (fun f -> if f > 0. then - f * log f else 0.)
+
+// chart examples
+let ex1 = [1;1;1;2;2;2;3;3;3] |> entropy id
+let ex2 = [1;1;1;1;1;1;1;1;1] |> entropy id
+let ex3 = [1;1;1;1;1;2;2;3;3] |> entropy id
+let ex4 = [1;1;1;1;1;1;1;2;3] |> entropy id
+
+let splitEntropy extractLabel extractFeature data =
+    // observations with no missing values for the selected feature
+    let dataWithValues =
+        data
+        |> Seq.filter (extractFeature |> hasData)
+
+    let size = dataWithValues |> Seq.length
+    
+    dataWithValues
+    |> Seq.groupBy extractFeature
+    |> Seq.sumBy (fun (_, group) ->
+        let groupSize = group |> Seq.length
+        let probaGroup = float groupSize / float size
+        let groupEntropy = group |> entropy extractLabel
+        probaGroup * groupEntropy)
+
+let survived (p:Passenger) = p.Survived
+let sex (p:Passenger) = Some(p.Sex)
+let pclass (p:Passenger) = Some(p.Pclass)
+let port (p:Passenger) =
+    if p.Embarked = "" 
+    then None
+    else Some(p.Embarked)
+let age (p:Passenger) =
+    if p.Age < 12.0
+    then Some("Younger")
+    else Some("Older")
+
+printfn "Comparison: most informative feature"
+let h = dataset.Rows |> entropy survived
+printfn "Base entropy %.3f" h
+
+dataset.Rows |> splitEntropy survived sex |> printfn "  Sex: %.3f"
+dataset.Rows |> splitEntropy survived pclass |> printfn "  Class: %.3f"
+dataset.Rows |> splitEntropy survived port |> printfn "  Port: %.3f"
+dataset.Rows |> splitEntropy survived age |> printfn "  Age: %.3f"
+
+let bySex = dataset.Rows |> Seq.groupBy sex
+for (groupName, group) in bySex do
+    printfn "Group: %s" groupName.Value
+    let h = group |> entropy survived
+    printfn "Base entropy %.3f" h
+
+    group |> splitEntropy survived sex |> printfn "  Sex: %.3f"
+    group |> splitEntropy survived pclass |> printfn "  Class: %.3f"
+    group |> splitEntropy survived port |> printfn "  Port: %.3f"
+    group |> splitEntropy survived age |> printfn "  Age: %.3f"
+
+// refactor to use same type for features
+let pclassString (p:Passenger) = Some(p.Pclass |> string)
+let features = 
+    [   "Sex", sex
+        "Class", pclassString   ]
+
+features
+|> List.map (fun (name, feat) ->
+    dataset.Rows
+    |> splitEntropy survived feat |> printfn "%s: %.3f" name)
---
+++ src/TreesAndForests/Titanic/Script.fsx
@@ -0,0 +1,147 @@
+﻿#I @"..\..\packages\"
+#r @"FSharp.Data.2.3.2\lib\net40\FSharp.Data.dll"
+
+open FSharp.Data
+
+[<Literal>]
+let path = "../Data/titanic.csv"
+
+type Titanic = CsvProvider<path>
+type Passenger = Titanic.Row
+
+let dataset = Titanic.GetSample()
+
+dataset.Rows
+|> Seq.countBy (fun passenger -> passenger.Survived)
+|> Seq.iter (printfn "%A")
+
+dataset.Rows
+|> Seq.averageBy (fun passenger ->
+    if passenger.Survived then 1.0 else 0.0)
+|> printfn "Chances of survival: %.3f"
+
+let survivalRate (passengers:Passenger seq) =
+    let total = passengers |> Seq.length
+    let survivors = 
+        passengers
+        |> Seq.filter (fun p -> p.Survived)
+        |> Seq.length
+    100.0 * (float survivors / float total)
+
+let survivedByFeature grouping print =
+    let grouping = 
+        dataset.Rows
+        |> Seq.groupBy grouping
+
+    grouping
+    |> Seq.iter (fun (s, g) ->
+        printfn "%s %A: %f" print s (survivalRate g))
+
+survivedByFeature (fun p -> p.Sex) "Sex"
+survivedByFeature (fun p -> p.Pclass) "Class"
+
+let mostFrequentLabelIn group =
+    group
+    |> Seq.countBy snd
+    |> Seq.maxBy snd
+    |> fst
+
+let learn sample extractFeature extractLabel =
+    // group together observations that have the
+    // same value for the selected feature, and
+    // find the most frequent label by group.
+    let groups =
+        sample
+        |> Seq.map (fun obs -> extractFeature obs, extractLabel obs)
+        |> Seq.groupBy fst
+        |> Seq.map (fun (feat, group) -> feat, mostFrequentLabelIn group)
+    // for an observation, find the group with
+    // matching feature value, and predict the
+    // most frequent label for that group.
+    let classifier obs =
+        let featureValue = extractFeature obs
+        groups
+        |> Seq.find (fun (f, _) -> f = featureValue)
+        |> snd
+    classifier
+
+let survived (p:Passenger) = p.Survived
+
+let sexClassifier = survived |> learn (dataset.Rows) (fun p -> p.Sex)
+
+printfn "Stump: classify based on passenger sex."
+dataset.Rows
+|> Seq.averageBy (fun p ->
+    if p.Survived = sexClassifier p then 1.0 else 0.0)
+|> printfn "Survived: %.3f"
+
+let classClassifier = survived |> learn (dataset.Rows) (fun p -> p.Pclass)
+
+printfn "Stump: classify based on passenger class."
+dataset.Rows
+|> Seq.averageBy (fun p ->
+    if p.Survived = classClassifier p then 1.0 else 0.0)
+|> printfn "Survived: %.3f"
+
+// using numbers for grouping by feature
+let survivalByPricePaid =
+    dataset.Rows
+    |> Seq.groupBy (fun p -> p.Fare)
+    |> Seq.iter (fun (price, passengers) ->
+        printfn "%6.2F: %6.2f" price (survivalRate passengers))
+
+let fareLevel =
+    let averageFare = 
+        dataset.Rows
+        |> Seq.averageBy (fun p -> p.Fare) 
+ 
+    fun (p:Passenger) ->
+        if p.Fare < averageFare 
+        then "Cheap"
+        else "Expensive" 
+
+let fareClassifier = survived |> learn (dataset.Rows) fareLevel
+
+printfn "Stump: classify based on fare level"
+dataset.Rows
+|> Seq.averageBy (fun p ->
+    if p.Survived = fareClassifier p then 1.0 else 0.0)
+|> printfn "Survived: %.3f"
+
+// missing data
+let survivalByPortOfOrigin =
+    dataset.Rows
+    |> Seq.groupBy (fun p -> p.Embarked)
+    |> Seq.iter (fun (port, passengers) ->
+        printfn "%s: %f" port (survivalRate passengers))
+
+let hasData extractFeature = extractFeature >> Option.isSome
+
+let betterLearn sample extractFeature extractLabel =
+    let branches =
+        sample
+        |> Seq.filter (extractFeature |> hasData)
+        |> Seq.map (fun obs -> extractFeature obs |> Option.get, extractLabel obs)
+        |> Seq.groupBy fst
+        |> Seq.map (fun (feat, group) -> feat, mostFrequentLabelIn group)
+        |> Map.ofSeq
+    let labelForMissingValues =
+        sample
+        |> Seq.countBy extractLabel
+        |> Seq.maxBy snd
+        |> fst
+    let classifier obs =
+        match (extractFeature obs) with 
+        | None -> labelForMissingValues
+        | Some(value) ->
+            match (branches.TryFind value) with
+            | None -> labelForMissingValues
+            | Some(predictedLabel) -> predictedLabel
+
+    classifier
+
+let port (p:Passenger) =
+    if p.Embarked = "" then None
+    else Some(p.Embarked)
+
+let updatedClassifier = survived |> betterLearn (dataset.Rows) port
